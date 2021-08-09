36220785a834d6ee047099b3d159fde19bd3d501
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2021-08-09 04:50:15 +0100

    adding the ability to create room - user can join the room - user can leave a thread - user can leave the room

---
+++ room.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+type room struct {
+	forward chan []byte
+	join chan *client
+	leave chan *client
+	clients map[*client]bool
+}
+
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join: make(chan *client),
+		leave: make(chan *client),
+		clientes: make(map[*client]bool),
+	}
+}
+
+func (r *room) run(){
+	for {
+		select {
+			case client := <-r.join:
+				// joining
+				r.clients[client] = true
+			case cloient := <-r.leave:
+				// leaving
+				delete(r.clientes, client)
+				close(client.send)
+			case msg := <-r.forward:
+				// forwarding message to all clients
+				for client := range r.clients {
+					client.send <- msg
+				}
+		}
+	}
+}
+
+const (
+	socketBufferSize = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request){
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("SendHTTP: ", err)
+		return
+	}
+	client := &client {
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r
+	}
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
+}
